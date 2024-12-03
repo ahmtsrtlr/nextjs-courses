18ebac64d7e743391390bc59fdd31250a58ae97f
ahmtsrtlr <a.sert97@hotmail.com>
2024-12-03 23:18:14 +0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 /** @format */
 
 const Home = () => (
-  <h1 className="h1-bold background-light800_dark400">
-    Welcome to the world of Next.js
-  </h1>
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the world of Next.js
+    </h1>
+  </>
 );
 
 export default Home;
